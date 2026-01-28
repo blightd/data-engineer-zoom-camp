fad7de5341a1d7dc10a6b430f86b9f74bfdffa43
blightd <128959665+blightd@users.noreply.github.com>
2026-01-28 11:51:35 +0000

    day 02

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+print("arguments", sys.argv)
+
+day = int(sys.argv[1])
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df['day'] = day
+print(df.head())
+
+df.to_parquet(f"output_day_{day}.parquet")
+print(f"Running pipeline for day {day}")
