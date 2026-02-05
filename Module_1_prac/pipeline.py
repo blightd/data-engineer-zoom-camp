122cf1b1c568218cbe589d898d5caffd42a68af9
blightd <128959665+blightd@users.noreply.github.com>
2026-02-05 12:18:09 +0000

    Revert last 3 commits: bad pipeline changes

--- Module_1_prac/pipeline.py
+++ Module_1_prac/pipeline.py
@@ -6,12 +6,7 @@ day = int(sys.argv[1])
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 df['day'] = day
 print(df.head())
-
 df.to_parquet(f"output_day_{day}.parquet")
-<<<<<<< HEAD
-print(f"Running pipeline for day {day}")
-=======
 print(f"Running pipeline for day {day}")
 
 # new confiles are add
->>>>>>> pipeline_branch_2
